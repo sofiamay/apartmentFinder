3c6cd9421564310baa180f1cd6238ec5e7d03952
Sofia Berlin <sofiamberlin@gmail.com>
2016-07-29 21:09:55 -0700

    Clean up house card styling

--- src/components/House.js
+++ src/components/House.js
@@ -6,14 +6,22 @@ export default class House extends Component {
     super(props, context);
     this.state = {
       images: false,
+      longDescription: false,
     };
+    // these bindings aren't working for some reason
     this.toggleImages = this.toggleImages.bind(this);
+    this.toggleLongDescription = this.toggleLongDescription.bind(this);
     this.defaultImage = this.defaultImage.bind(this);
     this.description = this.description.bind(this);
   }
   toggleImages() {
     this.setState({
-      images: true,
+      images: !this.state.images,
+    });
+  }
+  toggleLongDescription() {
+    this.setState({
+      longDescription: !this.state.longDescription,
     });
   }
   defaultImage() {
@@ -25,10 +33,19 @@ export default class House extends Component {
     }
     return defaultImage;
   }
+  /* the methods won't properly bind, even though I bound them in the constructor.
+  So I cheated by passing the component as a parameter
+  */
   description() {
     let description;
     try {
-      description = this.props.data.description.substring(0,400);
+      const text = this.props.data.description;
+      if (text.length > 400) {
+        description = `${text.substring(0, 400)}...`;
+      } else {
+        description = text;
+      }
+      // description = this.props.data.description.substring(0,400);
     } catch (err) {
       description = '';
     }
@@ -37,18 +54,18 @@ export default class House extends Component {
 
   render() {
     const defaultImage = this.defaultImage();
-    const description = this.description();
-    const cardHeight = { height: this.state.images ? '800px' : '400px' };
+    const cardHeight = { height: this.state.longDescription ? '800px' : '400px' };
     return (
       <div className="card" style={cardHeight}>
         { defaultImage ? <img src={defaultImage} /> : null}
         <div className="house-header">
-          <a onClick={() => {this.setState({ images: !this.state.images });}} href="#">
+          <a onClick={() => {this.setState({ images: !this.state.images });}} >
             {this.props.data.name}
           </a>
         </div>
-        <div className="house-description">{description}</div>
-        {  this.state.images ? <HouseImages images={this.props.data.images}/> : null }
+        { /* <div className="house-description">{description}</div> */}
+        <div className="house-description">{this.description()}</div>
+        { this.state.images ? <HouseImages images={this.props.data.images}/> : null }
       </div>
     );
   }
