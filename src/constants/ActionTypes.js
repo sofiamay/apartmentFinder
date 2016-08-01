b0f001e9a20dec87c0ab1cea7c65ad8b0cf2c6da
Sofia Berlin <sofiamberlin@gmail.com>
2016-08-01 16:52:32 -0700

    Clean up files

--- src/constants/ActionTypes.js
+++ src/constants/ActionTypes.js
@@ -3,8 +3,6 @@
  * within your app. Combined with the DevTools/logger, you can see how state and subsequently
  * your UI is being affected.
  */
-export const INCREMENT_COUNTER = 'INCREMENT_COUNTER';
-export const DECREMENT_COUNTER = 'DECREMENT_COUNTER';
 export const REQUEST_POSTS = 'REQUEST_POSTS';
 export const RECEIVE_POSTS = 'RECEIVE_POSTS';
 export const THROW_POSTS_ERROR = 'THROW_POSTS_ERROR';
